99afee05f52a5dce68105324925de02d5ac9496d
postmodern <postmodern.mod3@gmail.com>
2010-05-17 02:40:10 -0700

    Added a meditation namespace.

--- javascripts/default.js
+++ javascripts/default.js
@@ -1,19 +1,27 @@
+var meditation = {
+  mode: false,
+
+  enter: function() {
+    $("#page").fadeOut("slow");
+
+    // lazy hack to popup the somafm.com DroneZone stream
+    window.location = 'http://somafm.com/startstream=dronezone.pls';
+
+    meditation.mode = true;
+  },
+
+  exit: function() {
+    $("#page").fadeIn("slow");
+
+    meditation.mode = false;
+  }
+};
+
 var defaultLayout = {
   setupMoon: function() {
     // hooks the click events for the moon
     // to enter/exit "meditation" mode
-    $("#moon").toggle(
-      function() {
-        $("#page").fadeOut("slow");
-
-        // lazy hack to popup the somafm.com DroneZone stream
-        window.location = 'http://somafm.com/startstream=dronezone.pls';
-      },
-      
-      function() {
-        $("#page").fadeIn("slow");
-      }
-    );
+    $("#moon").toggle(meditation.enter,meditation.exit);
   }
 };
 
