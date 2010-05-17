d6d2fcabdf3a2e9031d8fd07daa250e2ccaa9ab4
postmodern <postmodern.mod3@gmail.com>
2010-05-17 04:18:57 -0700

    Added the meditation.stream variable.
    * Also added more comments.

--- javascripts/default.js
+++ javascripts/default.js
@@ -1,19 +1,28 @@
 var meditation = {
   mode: false,
 
+  stream: "http://somafm.com/startstream=dronezone.pls",
+
   enter: function() {
+    // imagine an endless white sand dune
+    // it is night time
+    // the moon is on the horizon
     $("#page").fadeOut("slow");
 
     // lazy hack to popup the somafm.com DroneZone stream
-    window.location = 'http://somafm.com/startstream=dronezone.pls';
+    window.location = meditation.stream;
 
     meditation.mode = true;
   },
 
   exit: function() {
+    // back to the noisy world
     $("#page").fadeIn("slow");
 
     meditation.mode = false;
+  },
+
+  playing: function() {
   }
 };
 
