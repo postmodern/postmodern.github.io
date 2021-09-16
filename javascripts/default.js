b5cbc4128fb2ee134b97e8ec6b9ceccb14c87af0
Postmodern <postmodern.mod3@gmail.com>
2021-09-15 18:47:56 -0700

    Added a custom dark-mode switch.

--- javascripts/default.js
+++ javascripts/default.js
@@ -4,4 +4,22 @@ var ready = (callback) => {
 }
 
 ready(() => {
+  const body = document.body;
+  const darkModeSwitch = document.getElementById("dark-mode-checkbox");
+
+  if (localStorage.getItem("dark-mode")) {
+    darkModeSwitch.checked = false; // turning the switch off == dark mode
+    body.classList.add("dark-mode");
+  }
+
+  darkModeSwitch.addEventListener("change", (event) => {
+    if (localStorage.getItem("dark-mode")) {
+      localStorage.removeItem("dark-mode");
+    }
+    else {
+      localStorage.setItem("dark-mode",true);
+    }
+
+    body.classList.toggle("dark-mode");
+  });
 });
