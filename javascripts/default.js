aa21084e1dc8f834343172f6631554acef5ed22d
Postmodern <postmodern.mod3@gmail.com>
2021-09-16 01:58:52 -0700

    Added JavaScript to mutate the header title.

--- javascripts/default.js
+++ javascripts/default.js
@@ -22,4 +22,38 @@ ready(() => {
 
     body.classList.toggle("dark-mode");
   });
+
+  const headerTitle = document.querySelector("header svg text > tspan");
+  var headerTitleIntervalID = null;
+
+  function startHeaderTitleMutator() {
+    if (headerTitleIntervalID == null) {
+      headerTitleIntervalID = setInterval(nextHeaderTitle, 60);
+    }
+  }
+
+  function stopHeaderTitleMutator() {
+    if (headerTitleIntervalID) {
+      clearInterval(headerTitleIntervalID);
+      headerTitleIntervalID = null;
+      setHeaderTitle("P O S T M O D E R N");
+    }
+  }
+
+  function randomDigit() { return Math.floor(Math.random() * 10).toString(); }
+  function setHeaderTitle(newTitle) { headerTitle.textContent = newTitle; }
+
+  function nextHeaderTitle() {
+    setHeaderTitle(
+      "P " + randomDigit() + " S T M " + randomDigit() + " D " + randomDigit() + " R N"
+    );
+  }
+
+  headerTitle.addEventListener("mouseenter", () => {
+    startHeaderTitleMutator();
+  });
+
+  headerTitle.addEventListener("mouseleave", () => {
+    stopHeaderTitleMutator();
+  });
 });
