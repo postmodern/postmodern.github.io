7238c838ddca81555c777addbe5028c6cbcb8d5c
Postmodern <postmodern.mod3@gmail.com>
2021-09-21 14:39:18 -0700

    Randomize every over letter.

--- javascripts/default.js
+++ javascripts/default.js
@@ -62,7 +62,7 @@ class HeaderTitle {
 
   randomize() {
     this.set(
-      `P ${Digits.random()} S T M ${Digits.random()} D ${Digits.random()} R N`
+      `P ${Digits.random()} S ${Digits.random()} M ${Digits.random()} D ${Digits.random()} R ${Digits.random()}`
     );
   }
 
